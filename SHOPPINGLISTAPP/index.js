233023a3c05849a4ec540705878ddee8440456c6
Alex Puhl <apuhlswimmer@hotmail.com>
2018-07-19 15:27:25 -0600

    finished shopping app

--- SHOPPINGLISTAPP/index.js
+++ SHOPPINGLISTAPP/index.js
@@ -1,56 +1,51 @@
 'use strict';
-/* global $ */
 
 
 
-const strikeItem = function(item) {
-  $(item).toggleClass('shopping-item__checked');
-};
 
-$('.shopping-item-toggle').click(function() {
-  // console.log(this);
-  const list = $(this).closest('li').find('span')[0];
+$('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+  console.log(this);
+  event.preventDefault();
+  let list = $(this).closest('li').find('span')[0];
   console.log(list);
-  // list.toggleClass('shopping-item__checked');
-  // strikeItem(list);
+  $(list).toggleClass('shopping-item__checked');
 });
 
 
 
-const deleteItem = function(item) {
-  $('this.closest(li)').remove();
-};
+$('.shopping-list').on('click', '.shopping-item-delete', function (event) {
+  console.log(this);
+  const deleteList = $(this).closest('li')[0];
+  console.log(deleteList);
+  $(deleteList).remove();
+});
 
 
 
+$('#js-shopping-list-form').on('submit', event => {
+  event.preventDefault();
+  const inputValue = $('.js-shopping-list-entry').val();
+  $('.js-shopping-list-entry').val('');
+  console.log(inputValue);
 
+  const htmlElement =
+    `
+    <li>
+      <span class="shopping-item">${inputValue}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>
+  `
+  $('.shopping-list').append(htmlElement);
 
+});
 
-
-
-
-
-
-
-
-
-
-
-const addItem = function(item) {
-
-};
-
-const handleSubmit = function () {
-  $('#js-shopping-list-form').on('submit', event => {
-    console.log(event.val);
-    event.preventDefault();
-    const countTo = $('#number-choice').val();
-
-
-
-
-  });
-};
 
 const shoppingList = $('.shopping-list');
 
